fcc9325135e9f2f240e8bc49292faedba79983c4
atsuo yamada <yamada-git@tir.jp>
2014-03-12 14:40:43 +0900

    releasing 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject packss "0.1.1-SNAPSHOT"
+(defproject packss "0.2.0"
   :description "pack/unpack shared-structure"
   :url "https://github.com/ayamada/packss"
   :license {:name "Eclipse Public License"
